34913f210af63b5515e398c88f7ee09e019d4e03
Yaroslav Malykhin <sk3llyar@gmail.com>
2022-03-22 17:15:53 +0200

    Added logger to redux

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger';
 import bandsReducer from '../reducers/bandReducer';
 
-const store = createStore(bandsReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(bandsReducer, applyMiddleware(thunkMiddleware, logger));
 
 export default store;
